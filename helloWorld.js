74cf987e6d0f3e2da34409e6f12367e30f8a6466
Andika <andikapujianto285@gmail.com>
2025-02-05 09:12:11 +0700

    adding exercise02

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,5 @@
 const hello = "Hello World";
+const home = "This is the home page";
 const greetings = () => "Hi, my name is Dika";
 
-module.exports = { hello, greetings };
+module.exports = { hello, home, greetings };
--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const http = require("http");
-const { greetings, hello } = require("./helloWorld");
+const { hello, home, greetings } = require("./helloWorld");
+const users = require("./users");
 const moment = require("moment");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.write(hello);
-  res.write(greetings());
-  res.write(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  // res.write(hello)
+  // res.write(greetings())
+  const url = req.url;
+  if (url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.write(home);
+  } else if (url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.write(
+      JSON.stringify({
+        status: "success",
+        message: "respone success",
+        description: "Exercise #02",
+        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      })
+    );
+  } else if (url === "/users") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.write(
+      JSON.stringify({
+        users: users,
+      })
+    );
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/json");
+    res.write(
+      JSON.stringify({
+        status: "not found",
+        message: "Route tidak ditemukan",
+        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      })
+    );
+  }
   res.end();
 });
 
 const hostname = "127.0.0.1";
 const port = 3000;
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}`)
+);
