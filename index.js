bb45198cb11c5847b6d3df27dd4502b3c9fc45a3
Andika <andikapujianto285@gmail.com>
2025-02-12 09:57:57 +0800

    adding exercise03: update exercise02 using express JS

--- index.js
+++ index.js
@@ -2,50 +2,30 @@ const http = require("http");
 const { hello, home, greetings } = require("./helloWorld");
 const users = require("./users");
 const moment = require("moment");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  // res.write(hello)
-  // res.write(greetings())
-  const url = req.url;
-  if (url === "/") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(home);
-  } else if (url === "/about") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "respone success",
-        description: "Exercise #02",
-        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      })
-    );
-  } else if (url === "/users") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        users: users,
-      })
-    );
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "not found",
-        message: "Route tidak ditemukan",
-        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      })
-    );
-  }
-  res.end();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json(home);
+});
+app.get("/about", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "response success",
+    description: "Exercise #03",
+    date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+  });
+});
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    users: users,
+  });
 });
 
 const hostname = "127.0.0.1";
 const port = 3000;
-server.listen(port, hostname, () =>
-  console.log(`Server running at http://${hostname}:${port}`)
-);
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
