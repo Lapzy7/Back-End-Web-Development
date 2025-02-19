a8493554a27607085c711285a76cee43d469877b
Andika <andikapujianto285@gmail.com>
2025-02-19 09:43:17 +0800

    adding exercise04

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
-const http = require("http");
-const { hello, greetings } = require("./helloWorld");
-const moment = require("moment");
 const express = require("express");
 const morgan = require("morgan");
-const errorhandler = require("errorhandler");
+const users = require("./users");
+
 const app = express();
 
-//Middleware
-const log = (req, res, next) => {
-  console.log(
-    moment().format("h:mm:ss a") + " " + req.originalUrl + " " + req.ip
-  );
-  next();
-};
-
+// Middleware
 app.use(morgan("tiny"));
-app.use(errorhandler);
 
-// Routing
-app.get("/", (req, res) => res.send("Hello World"));
-app.get("/about", (req, res) =>
-  res.status(200).json({
-    status: "success",
-    message: "About page",
-    data: [],
-  })
-);
-app.post("/contoh", (req, res) => res.send("request method POST"));
-app.put("/contoh", (req, res) => res.send("Request method PUT"));
-app.delete("/contoh", (req, res) => res.send("Request method DELETE"));
-app.patch("/contoh", (req, res) => res.send("Request method PATCH"));
-
-app.all("/universal", (req, res) => res.send(`Request method ${req.method}`));
-// Routing dinamis
-// 1. Menggunakan params
-app.get("/post/:id", (req, res) => res.send(`Artikel ke - ${req.params.id}`));
-// 2. Menggunakan Query String
-app.get("/post", (req, res) => {
-  const { page, sort } = req.query;
-  res.send(`Query string= page :${page}, sort : ${sort}`);
+// Routes
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
-//Middleware untuk 404
+app.get("/users/:name", (req, res) => {
+  const searchName = req.params.name.toLowerCase();
+  const user = users.find((user) => user.name.toLowerCase() === searchName);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Data user tidak ditemukan",
+    });
+  }
+
+  res.json(user);
+});
+
+// 404 route handler
 app.use((req, res, next) => {
   res.status(404).json({
     status: "error",
     message: "resource tidak ditemukan",
+  });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: "error",
+    message: "terjadi kesalahan pada server",
   });
 });
 
