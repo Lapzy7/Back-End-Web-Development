4febb4db7a3b95571aa3b7ad01f6a5d868d9e0b5
Andika <andikapujianto285@gmail.com>
2025-03-03 09:04:11 +0800

    adding pertemuan12: file preview & download, middleware static file

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const log = (req, res, next) => {
 app.use(morgan("tiny"));
 // app.use(errorhandler);
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 //Routing
 app.use(routers);
